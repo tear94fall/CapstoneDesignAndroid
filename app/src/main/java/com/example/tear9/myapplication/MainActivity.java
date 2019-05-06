a6878936c7969b18bfee34f63af4e5e7dd533a94
tear94fall <tear94fall@naver.com>
2019-05-06 19:22:54 +0900

    1. 서버와 통신을 통한 로그인 성공 (입력값을 보낸뒤 처리결과만을 서버에게 받아옴) 2. 모든 것이 정상작동하므로, ui만 수정할것

--- app/src/main/java/com/example/tear9/myapplication/MainActivity.java
+++ app/src/main/java/com/example/tear9/myapplication/MainActivity.java
@@ -9,8 +9,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.tear9.myapplication.MsgPacker.Client;
 
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         Button button = findViewById(R.id.button);
         Button reg_button = findViewById(R.id.Register);
 
+        // 로그인 버튼 눌렀을 경우
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -31,25 +35,39 @@ public class MainActivity extends AppCompatActivity {
                 TextView textView1 = (TextView) findViewById(R.id.textView4);
 
                 // 로그인 성공 여부 확인 하는 함수
-                if (!logInCheck(id_str, pw_str)) {
-                    /* 로그인 성공시 페이지 전환 */
-                    Intent intent = new Intent(getApplicationContext(), FinishActivity.class);
-                    startActivity(intent);
-                    finish();
-                }else{
-                    /* 로그인 실패시 현재 페이지 에서 팝업창 띄울것 */
-                    /* 일정 횟수 이상 넘어갈 경우에는 블로킹 되도록 처리 함*/
-                    new AlertDialog.Builder(MainActivity.this)
-                            .setTitle("로그인 실패")
-                            .setMessage("올바르지 않은 시도입니다.")
-                            .setNeutralButton("다시 시도", new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dlg, int sumthin) {
-                                }
-                            }).show(); // 팝업창 보여줌
+                try {
+                    String result[] = logInCheck(id_str, pw_str);
+                    boolean checker = false;
+
+                    if("true".equals(result[0])) {
+                        checker = true;
+                    }
+
+                    if(checker) {
+                        /* 로그인 성공시 페이지 전환 */
+                        Intent intent = new Intent(getApplicationContext(), FinishActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }else{
+                        /* 로그인 실패시 현재 페이지 에서 팝업창 띄울것 */
+                        /* 일정 횟수 이상 넘어갈 경우에는 블로킹 되도록 처리 함*/
+                        new AlertDialog.Builder(MainActivity.this)
+                                .setTitle("로그인 실패")
+                                .setMessage("아디이와 비밀번호를 확인 해주세요")
+                                .setNeutralButton("다시 시도", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dlg, int sumthin) {
+                                    }
+                                }).show(); // 팝업창 보여줌
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             }
         });
 
+        // 회원가입 버튼이 눌렸을 경우
         reg_button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -61,8 +79,25 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    protected boolean logInCheck(String id, String passwd){
-        String send_message = id + passwd;
-        return true;
+    protected String[] logInCheck(final String _id, final String _passwd) throws IOException, InterruptedException {
+        final String[] result = new String[1];
+        new Thread() {
+            public void run() {
+                try {
+                    Client client = new Client();
+                    client.setLoginInfo(_id, _passwd);
+                    client.loginCheck();
+                    result[0] = client.getResponse_data();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+
+        Thread.sleep(1000);
+        return result;
     }
 }
