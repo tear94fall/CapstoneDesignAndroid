0226344164ad0aeba37d4e4f519dddd622bcf6a4
tear94fall <tear94fall@naver.com>
2019-05-08 22:35:45 +0900

    1. 회원가입 정상적으로 작동함. 2. 아이디 중복확인과 비밀번호 확인과 비밀번호 확인값이 서로 일치 하지 않으면 회원가입 되지 않음. 3. 회원가입 완료시 로그인 화면으로 이동 4. 기타 에러 처리 세분화함 (팝업알림)

--- app/src/main/java/com/example/tear9/myapplication/RegisterActivity.java
+++ app/src/main/java/com/example/tear9/myapplication/RegisterActivity.java
@@ -16,7 +16,7 @@ import java.io.IOException;
 import static java.lang.Thread.sleep;
 
 public class RegisterActivity extends AppCompatActivity {
-    boolean ID_checker = false;
+    String ID_checker = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +29,7 @@ public class RegisterActivity extends AppCompatActivity {
 
 
         // 아이디 중복 검사를 위한 변수
-        ID_checker = false;
+        ID_checker = "none";
 
         final TextView input_id = (TextView) findViewById(R.id.UserIDInput);
         final TextView input_pw = (TextView) findViewById(R.id.UserPWInput);
@@ -71,11 +71,19 @@ public class RegisterActivity extends AppCompatActivity {
 
                 // 중복 아이디가 존재 하지 않을때
                 if (!("true".equals(result[0]))) {
-                    ID_checker = true;
+                    ID_checker = "success";
 
                     // 중복 아이디가 없으면 그대로 로직 수행
+                    new AlertDialog.Builder(RegisterActivity.this)
+                            .setTitle("사용할수 있는 아이디 입니다.")
+                            .setMessage("계속 회원가입을 진행 해주세요")
+                            .setNeutralButton("계속 진행하기", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dlg, int sumthin) {
+                                }
+                            }).show(); // 팝업창 보여줌
                 } else {
                     // 중복 아이디 존재
+                    ID_checker = "exist";
                     new AlertDialog.Builder(RegisterActivity.this)
                             .setTitle("이미 등록된 아이디 입니다.")
                             .setMessage("새로운 아이디를 입력해주세요")
@@ -91,10 +99,17 @@ public class RegisterActivity extends AppCompatActivity {
         Register_request_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                TextView input_id = (TextView) findViewById(R.id.UserIDInput);
+                TextView input_pw = (TextView) findViewById(R.id.UserPWInput);
+                TextView input_pw_check = (TextView) findViewById(R.id.userPWCheckInput);
+                TextView input_name = (TextView) findViewById(R.id.UserNameInput);
+                TextView input_tel = (TextView) findViewById(R.id.userTelInput);
 
                 String id = input_id.getText().toString();
                 String pw = input_pw.getText().toString();
+                String pw_check = input_pw_check.getText().toString();
+                String name = input_name.getText().toString();
+                String tel = input_tel.getText().toString();
 
                 /*
                 회원 가입이 안되야 하는 경우
@@ -104,36 +119,106 @@ public class RegisterActivity extends AppCompatActivity {
                 3. 비밀번호 빈값
                  */
 
-                if(ID_checker){
-                    //회원 가입 진행
-                }else{
-                    //아이디가 이미 존재하는 등과 같은 기타 오류
-                }
+                if("success".equals(ID_checker)){
+                    //회원 가입 진행 하는데 모든 경우의 수를 다 통과해야 회원가입 진행
 
+                    // 비밀 번호가 다를 경우 팝업창을 띄움
+                    if(!pw.equals(pw_check)){
+                        if (pw.trim().length() == 0 || pw_check.trim().length()==0) {
+                            // 패스워드가 빈값을 경우
+                            new AlertDialog.Builder(RegisterActivity.this)
+                                    .setTitle("비밀번호를 입력해주세요")
+                                    .setMessage("올바른 비밀번호를 입력해주세요")
+                                    .setNeutralButton("다시 시도", new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dlg, int sumthin) {
+                                        }
+                                    }).show(); // 팝업창 보여줌
+                        }else {
+                            new AlertDialog.Builder(RegisterActivity.this)
+                                    .setTitle("비밀번호가 일치하지 않습니다.")
+                                    .setMessage("비밀번호를 일치시켜 주세요")
+                                    .setNeutralButton("다시 시도", new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dlg, int sumthin) {
+                                        }
+                                    }).show(); // 팝업창 보여줌
+                        }
+                    }else if (id.trim().length() == 0) {
+                        // 아이디가 빈값일 경우
+                        new AlertDialog.Builder(RegisterActivity.this)
+                                .setTitle("아이디를 입력해주세요")
+                                .setMessage("올바른 아이디를 입력해주세요")
+                                .setNeutralButton("다시 시도", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dlg, int sumthin) {
+                                    }
+                                }).show(); // 팝업창 보여줌
+                    } else if (name.trim().length() == 0){
+                        new AlertDialog.Builder(RegisterActivity.this)
+                                .setTitle("이름을 입력해주세요")
+                                .setMessage("올바른 이름을 입력해주세요")
+                                .setNeutralButton("다시 시도", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dlg, int sumthin) {
+                                    }
+                                }).show(); // 팝업창 보여줌
+                    }else if (tel.trim().length() == 0){
+                        new AlertDialog.Builder(RegisterActivity.this)
+                                .setTitle("전화번호를 입력해주세요")
+                                .setMessage("올바른 전화번호를 입력해주세요")
+                                .setNeutralButton("다시 시도", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dlg, int sumthin) {
+                                    }
+                                }).show(); // 팝업창 보여줌
+                    }else{
+                        // 회원 가입 진행함
+                        String result[] = new String[1];
+                        try{
+                            // 회원가입 처리 시작
+                            result = CreateAccount(id, pw, name, tel);
 
-                // 아이디가 빈값일 경우
-                if (id.trim().length() == 0) {
-                    new AlertDialog.Builder(RegisterActivity.this)
-                            .setTitle("아이디를 입력해주세요")
-                            .setMessage("올바른 아이디를 입력해주세요")
-                            .setNeutralButton("다시 시도", new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dlg, int sumthin) {
-                                }
-                            }).show(); // 팝업창 보여줌
-
-                    // 패스워드가 빈값을 경우
-                } else if (pw.trim().length() == 0) {
-                    new AlertDialog.Builder(RegisterActivity.this)
-                            .setTitle("비밀번호를 입력해주세요")
-                            .setMessage("올바른 비밀번호를 입력해주세요")
-                            .setNeutralButton("다시 시도", new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dlg, int sumthin) {
-                                }
-                            }).show(); // 팝업창 보여줌
-                }
-
-                if (ID_checker) {
-                    // 서버에 값을 보내는 로직 추가할것
+                            // 회원가입 결과 처리 시작
+                            if ("true".equals(result[0])) {
+                                // 회원가입에 성공했을 경우 로그인 화면으로 이동함
+                                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                startActivity(intent);
+                                finish();
+                            } else {
+                                // 회원가입에 실패 했을경우
+                                new AlertDialog.Builder(RegisterActivity.this)
+                                        .setTitle("회원 가입 실패.")
+                                        .setMessage("다시 한번 진행해 주세요")
+                                        .setNeutralButton("다시 시도", new DialogInterface.OnClickListener() {
+                                            public void onClick(DialogInterface dlg, int sumthin) {
+                                            }
+                                        }).show(); // 팝업창 보여줌
+                            }
+                        } catch (Exception e){
+                            new AlertDialog.Builder(RegisterActivity.this)
+                                    .setTitle("회원 가입 실패.")
+                                    .setMessage("다시 한번 진행해 주세요")
+                                    .setNeutralButton("다시 시도", new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dlg, int sumthin) {
+                                        }
+                                    }).show(); // 팝업창 보여줌
+                        }
+                    }
+                }else {
+                    // 중복 아이디 존재
+                    if("exist".equals(ID_checker)) {
+                        new AlertDialog.Builder(RegisterActivity.this)
+                                .setTitle("이미 등록된 아이디 입니다.")
+                                .setMessage("새로운 아이디를 입력해주세요")
+                                .setNeutralButton("다시 시도", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dlg, int sumthin) {
+                                    }
+                                }).show(); // 팝업창 보여줌
+                    }else if ("none".equals(ID_checker)){
+                        new AlertDialog.Builder(RegisterActivity.this)
+                                .setTitle("존재하는 아이디 인지 확인해주세요.")
+                                .setMessage("아이디 중복검사를 해주세요")
+                                .setNeutralButton("다시 시도", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dlg, int sumthin) {
+                                    }
+                                }).show(); // 팝업창 보여줌
+                    }
                 }
             }
         });
@@ -160,4 +245,23 @@ public class RegisterActivity extends AppCompatActivity {
         return result;
     }
 
+    protected String[] CreateAccount(final String _id, final String _passwd, final String _name, final String _tel) throws IOException, InterruptedException {
+        final String[] result = new String[1];
+        new Thread() {
+            public void run() {
+                try {
+                    Client client = new Client();
+                    client.CreateAccount(_id, _passwd, _name, _tel);
+                    result[0] = client.getResponse_data();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+
+        sleep(1000);
+        return result;
+    }
 }
