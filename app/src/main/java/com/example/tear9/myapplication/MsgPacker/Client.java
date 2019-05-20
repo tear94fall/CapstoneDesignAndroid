ca7445041bfa54ad4f772c01f957ea3d6c440f13
tear94fall <tear94fall@naver.com>
2019-05-21 02:50:18 +0900

    1.캡챠(이미지에 글자있는 테스트) 메소드 작성함

--- app/src/main/java/com/example/tear9/myapplication/MsgPacker/Client.java
+++ app/src/main/java/com/example/tear9/myapplication/MsgPacker/Client.java
@@ -13,6 +13,10 @@ public class Client {
     private String userid;
     private String passwd;
     private String response_data;
+    private String emulator_ip_addr = "10.0.2.2";
+    private String server_target_ip = "172.17.211.117";
+    /* 아이피 설정을 위한 부분 */
+    private String ip_addr = emulator_ip_addr;
 
     public void setLoginInfo(String id, String passwd){
         this.userid = id;
@@ -29,10 +33,8 @@ public class Client {
         Scanner scan = new Scanner(System.in);
 
         try {
-            InetSocketAddress hostAddress = new InetSocketAddress("10.0.2.2", 8888);
-            System.out.println("Client Started!");
+            InetSocketAddress hostAddress = new InetSocketAddress(ip_addr, 8888);
             SocketChannel client = SocketChannel.open(hostAddress);
-            System.out.println("Client Started!");
 
             MessagePacker msg = new MessagePacker();
             DataBuffer DataBuf = new DataBuffer();
@@ -159,6 +161,214 @@ public class Client {
             DataBuf.set_data("user_name", user_name);
             DataBuf.set_data("user_tel", user_tel);
 
+            packet = DataBuf.get_data();
+
+            msg.add(packet);
+            msg.Finish();
+
+            /* 서버에게 데이터를 보냄 */
+            client.write(msg.getBuffer());
+
+            ByteBuffer buf = ByteBuffer.allocateDirect(1024);
+
+            /* 서버에서 데이터를 받아옴 */
+            client.read(buf);
+
+            /* ByteBuffer 를 String 으로 저장 함*/
+            byte[] bytes = new byte[buf.position()];
+            buf.flip();
+            buf.get(bytes);
+            String recv_message = new String(bytes);
+
+
+            /* 반드시 닫아줄것 */
+            client.close();
+
+            /* 에러 처리 로직 추가 할것 */
+
+            response_data = recv_message;
+
+        }catch (UnknownHostException ex) {
+            System.err.println(ex);
+        }catch (IOException ex) {
+            System.err.println(ex);
+        }
+    }
+
+
+    //캡챠의 문제를 가져옴
+    public void getCaptchatTestSet() throws UnknownHostException,IOException, InterruptedException {
+        try {
+            InetSocketAddress hostAddress = new InetSocketAddress("10.0.2.2", 8888);
+            SocketChannel client = SocketChannel.open(hostAddress);
+
+            MessagePacker msg = new MessagePacker();
+            DataBuffer DataBuf = new DataBuffer();
+
+            String packet;
+            String request_number="10";
+
+            DataBuf.set_data("request_number", request_number);
+            packet = DataBuf.get_data();
+
+            msg.add(packet);
+            msg.Finish();
+
+            /* 서버에게 데이터를 보냄 */
+            client.write(msg.getBuffer());
+
+            ByteBuffer buf = ByteBuffer.allocateDirect(1024);
+
+            /* 서버에서 데이터를 받아옴 */
+            client.read(buf);
+
+            /* ByteBuffer 를 String 으로 저장 함*/
+            byte[] bytes = new byte[buf.position()];
+            buf.flip();
+            buf.get(bytes);
+            String recv_message = new String(bytes);
+
+
+            /* 반드시 닫아줄것 */
+            client.close();
+
+            /* 에러 처리 로직 추가 할것 */
+
+            response_data = recv_message;
+
+        }catch (UnknownHostException ex) {
+            System.err.println(ex);
+        }catch (IOException ex) {
+            System.err.println(ex);
+        }
+    }
+
+
+    // 캡챠의 정답을 체크 하는 메소드
+    public void CreateAccount(String _captcha_ans_num, String _capthca_ans) throws UnknownHostException,IOException, InterruptedException {
+        try {
+            InetSocketAddress hostAddress = new InetSocketAddress("10.0.2.2", 8888);
+            SocketChannel client = SocketChannel.open(hostAddress);
+
+            MessagePacker msg = new MessagePacker();
+            DataBuffer DataBuf = new DataBuffer();
+
+            String packet;
+            String request_number="10";
+
+            String captcha_ans_num = _captcha_ans_num;
+            String captcha_ans = _capthca_ans;
+
+            DataBuf.set_data("request_number", request_number);
+            DataBuf.set_data("ans_num", captcha_ans_num);
+            DataBuf.set_data("ans", captcha_ans);
+
+            packet = DataBuf.get_data();
+
+            msg.add(packet);
+            msg.Finish();
+
+            /* 서버에게 데이터를 보냄 */
+            client.write(msg.getBuffer());
+
+            ByteBuffer buf = ByteBuffer.allocateDirect(1024);
+
+            /* 서버에서 데이터를 받아옴 */
+            client.read(buf);
+
+            /* ByteBuffer 를 String 으로 저장 함*/
+            byte[] bytes = new byte[buf.position()];
+            buf.flip();
+            buf.get(bytes);
+            String recv_message = new String(bytes);
+
+
+            /* 반드시 닫아줄것 */
+            client.close();
+
+            /* 에러 처리 로직 추가 할것 */
+
+            response_data = recv_message;
+
+        }catch (UnknownHostException ex) {
+            System.err.println(ex);
+        }catch (IOException ex) {
+            System.err.println(ex);
+        }
+    }
+
+    // 마지막 운전 날짜를 가져오는 메소드
+    public void getLastDriveDate(String _id) throws UnknownHostException,IOException, InterruptedException {
+        Scanner scan = new Scanner(System.in);
+
+        try {
+            InetSocketAddress hostAddress = new InetSocketAddress(ip_addr, 8888);
+            SocketChannel client = SocketChannel.open(hostAddress);
+
+            MessagePacker msg = new MessagePacker();
+            DataBuffer DataBuf = new DataBuffer();
+
+            //msg.SetProtocol(MessageProtocol.CHAT);
+            // 위의 코드를 실행하면 처음에 프로토콜을 전송데이터의 처음에 삽입함
+
+            String packet;
+            String request_number="12";
+            String user_id=_id;
+
+            DataBuf.set_data("request_number", request_number);
+            DataBuf.set_data("user_id", user_id);
+
+            packet = DataBuf.get_data();
+            System.out.println(packet);
+
+            msg.add(packet);
+            msg.Finish();
+
+            /* 서버에게 데이터를 보냄 */
+            client.write(msg.getBuffer());
+
+            ByteBuffer buf = ByteBuffer.allocateDirect(1024);
+
+            /* 서버에서 데이터를 받아옴 */
+            client.read(buf);
+
+            /* ByteBuffer 를 String으로 저장 함*/
+            byte[] bytes = new byte[buf.position()];
+            buf.flip();
+            buf.get(bytes);
+            String recv_message = new String(bytes);
+
+
+            /* 반드시 닫아줄것 */
+            client.close();
+
+            /* 에러 처리 로직 추가 할것 */
+
+            response_data = recv_message;
+
+        }catch (UnknownHostException ex) {
+            System.err.println(ex);
+        }catch (IOException ex) {
+            System.err.println(ex);
+        }
+    }
+
+    // 이미지에 쓰여진 글자를 통해 테스트를 진행하는 캡차
+    // 입력된 정답과 문제를 보냄 테스트가 통과했는지 아닌지에대한 값만을 가져옴
+    public void getCaptchatTestSet2(String captcha_test_number, String captcha_test_answer) throws UnknownHostException,IOException, InterruptedException {
+        try {
+            InetSocketAddress hostAddress = new InetSocketAddress("10.0.2.2", 8888);
+            SocketChannel client = SocketChannel.open(hostAddress);
+
+            MessagePacker msg = new MessagePacker();
+            DataBuffer DataBuf = new DataBuffer();
+
+            String packet;
+            String request_number="14";
+
+            DataBuf.set_data("request_number", request_number);
+            DataBuf.set_data("captcha2_number", captcha_test_number);
+            DataBuf.set_data("captcha2_answer", captcha_test_answer);
             packet = DataBuf.get_data();
 
             msg.add(packet);
