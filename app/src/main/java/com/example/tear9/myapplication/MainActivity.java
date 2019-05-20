5f2622a5982b009bd8db1421ad07b069423a7f2a
tear94fall <tear94fall@naver.com>
2019-05-21 02:52:13 +0900

    1. 마지막으로 접속한 날짜 띄우는 팝업 수정할것 2. 현재는 바로 테스트 시작 페이지로 이동함

--- app/src/main/java/com/example/tear9/myapplication/MainActivity.java
+++ app/src/main/java/com/example/tear9/myapplication/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.os.SystemClock;
 
 import com.example.tear9.myapplication.MsgPacker.Client;
 
@@ -16,10 +17,16 @@ import java.io.IOException;
 import static java.lang.Thread.sleep;
 
 public class MainActivity extends AppCompatActivity {
+    /* 중복 클릭 방지를 위한 변수 */
+    private static final long MIN_CLICK_INTERVAL = 1000;
+    private long mLastClickTime;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // 상단 타이틀 변경
+        setTitle("Login");
 
         final Button button = findViewById(R.id.button);
         Button reg_button = findViewById(R.id.Register);
@@ -30,6 +37,16 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                long currentClickTime = SystemClock.uptimeMillis();
+                long elapsedTime = currentClickTime - mLastClickTime;
+                mLastClickTime = currentClickTime;
+
+                // 중복 클릭인 경우
+                if (elapsedTime <= MIN_CLICK_INTERVAL) {
+                    return;
+                }
+
                 TextView id = (TextView) findViewById(R.id.editText2);
                 TextView pw = (TextView) findViewById(R.id.editText);
 
@@ -43,16 +60,40 @@ public class MainActivity extends AppCompatActivity {
                     String result[] = logInCheck(id_str, pw_str);
                     boolean checker = false;
 
-                    if("true".equals(result[0])) {
+                    if ("true".equals(result[0])) {
                         checker = true;
                     }
 
-                    if(checker) {
+                    if (checker) {
                         /* 로그인 성공시 페이지 전환 */
+                        final boolean[] pop_up_checker = new boolean[1];
+                        pop_up_checker[0] = false;
+
+                        /* 로그인 성공한 다음 */
+                        /* 마지막 운전 날짜를 알려줌 */
+                        String last_drive_date[] = lastDriverDate(id_str);
+
+                        /*
+                        if (!("false".equals(last_drive_date[0]))) {
+                            // 로그인 성공 팝업. 마지막 접속 날짜로 부터 몇일이 지났는지 표시 할것
+
+                            new AlertDialog.Builder(MainActivity.this)
+                                    .setTitle("로그인 성공!!")
+                                    .setMessage("환영 합니다")
+                                    .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dlg, int sumthin) {
+                                            pop_up_checker[0] = true;
+                                        }
+                                    }).show(); // 팝업창 보여줌
+                        }
+                        */
+
+                        /* 테스트 시작 페이지로 이동함*/
                         Intent intent = new Intent(getApplicationContext(), FinishActivity.class);
                         startActivity(intent);
                         finish();
-                    }else {
+
+                    } else {
                         /* 로그인 실패시 현재 페이지 에서 팝업창 띄울것 */
                         /* 일정 횟수 이상 넘어갈 경우에는 블로킹 되도록 처리 함*/
                         login_cnt[0] += 1;
@@ -90,7 +131,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         // 회원가입 버튼이 눌렸을 경우
-        reg_button.setOnClickListener(new View.OnClickListener(){
+        reg_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 /* 회원가입 버튼이 눌렸을 경우에는 무조건 회원 가입 페이지로 화면 전환 */
@@ -101,6 +142,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    // 서버에게 보내는 로그인 요청 메소드
     protected String[] logInCheck(final String _id, final String _passwd) throws IOException, InterruptedException {
         final String[] result = new String[1];
         new Thread() {
@@ -109,6 +151,28 @@ public class MainActivity extends AppCompatActivity {
                     Client client = new Client();
                     client.setLoginInfo(_id, _passwd);
                     client.loginCheck();
+                    result[0] = client.getResponse_data();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+
+        sleep(1000);
+        return result;
+    }
+
+    // 서버에게 보내는 마지막 운전 날짜 요청 메소드
+    protected String[] lastDriverDate(final String _id) throws IOException, InterruptedException {
+        final String[] result = new String[1];
+        new Thread() {
+            public void run() {
+                try {
+                    Client client = new Client();
+                    client.getLastDriveDate(_id);
                     result[0] = client.getResponse_data();
 
                 } catch (IOException e) {
