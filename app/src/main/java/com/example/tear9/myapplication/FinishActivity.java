3c091e138c5df78001762dd172034a6a5f23b698
tear94fall <tear94fall@naver.com>
2019-05-21 02:51:30 +0900

    테스트 시작 버튼을 누르면 램덤으로 테스트액티비티로 이동

--- app/src/main/java/com/example/tear9/myapplication/FinishActivity.java
+++ app/src/main/java/com/example/tear9/myapplication/FinishActivity.java
@@ -1,5 +1,7 @@
 package com.example.tear9.myapplication;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -16,12 +18,30 @@ public class FinishActivity extends AppCompatActivity {
 
         Button button = findViewById(R.id.button2);
 
+        new AlertDialog.Builder(FinishActivity.this)
+                .setTitle("로그인 성공!!")
+                .setMessage("환영합니다.")
+                .setNeutralButton("확인", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dlg, int sumthin) {
+                    }
+                }).show(); // 팝업창 보여줌
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), JoinActivity.class);
-                startActivity(intent);
-                finish();
+                int number;
+                number=(int)(Math.random()*100);
+                number%=2;
+
+                if(number==0) {
+                    Intent intent = new Intent(getApplicationContext(), JoinActivity.class);
+                    startActivity(intent);
+                    finish();
+                }else{
+                    Intent intent = new Intent(getApplicationContext(), Captcah2Activity.class);
+                    startActivity(intent);
+                    finish();
+                }
             }
         });
     }
